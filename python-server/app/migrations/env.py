c3587b1d87d37c7d7dfd7a4b90fd7683664dda87
Ajna Mujkic <amujkic1@etf.unsa.ba>
2026-04-17 22:29:57 +0200

    fix: connection string

--- python-server/app/migrations/env.py
+++ python-server/app/migrations/env.py
@@ -16,10 +16,13 @@ from models.profile_tag import ProfileTag
 from models.profile import Profile
 from models.tag import Tag
 from models.unavailable_period import UnavailablePeriod
+from dotenv import load_dotenv
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
