139a45071697c62f76885691424387b665289c86
Ajna Mujkic <amujkic1@etf.unsa.ba>
2026-05-12 22:09:10 +0200

    updated cors

--- python-server/app/main.py
+++ python-server/app/main.py
@@ -20,13 +20,19 @@ from app.api.industry import router as industry_router
 from app.api.company_type import router as company_type_router
 from app.api.unavailable_period import router as unavailable_period_router
 from app.api.deps import get_current_user
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
