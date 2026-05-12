1b9059e1c2096dd2a04b290030cc5deef59a0fb6
Ajna Mujkic <amujkic1@etf.unsa.ba>
2026-05-12 11:23:21 +0200

    feat: added new schemas for register

--- python-server/app/schemas/user.py
+++ python-server/app/schemas/user.py
@@ -2,9 +2,20 @@ from pydantic import BaseModel, EmailStr, Field
 from uuid import UUID
 from datetime import datetime
 
+from app.schemas.company import CompanyBase
+from app.schemas.profile import ProfileBase
+
 class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=8, max_length=72, description="Password must be between 8 and 72 characters")
+
+class CompanyRegistrationCreate(UserCreate):
+    profile: ProfileBase
+    company: CompanyBase
+
+class InvitationRegistrationCreate(UserCreate):
+    invitation_token: str
+    profile: ProfileBase
 
 class UserRead(BaseModel):
     id: UUID
