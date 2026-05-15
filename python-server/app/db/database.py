9b31398afdac195d0b56c7188e74c05839b79e04
Ajna Mujkic <amujkic1@etf.unsa.ba>
2026-05-15 18:27:41 +0200

    fix: async driver

--- python-server/app/db/database.py
+++ python-server/app/db/database.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 from sqlalchemy import DateTime, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from app.core.config import settings 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+elif SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgresql://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
@@ -32,4 +37,4 @@ class Base(DeclarativeBase):
 async def get_db():
     async with SessionLocal() as session:
         yield session
-        # No need for explicit close; 'async with' handles it
+        # 'async with' handles the close automatically
